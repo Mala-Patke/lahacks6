eb7900a9c4bbbe238641b7fee53b11de958a7d62
VarinSikka <88629852+VarinSikka@users.noreply.github.com>
2022-09-25 08:08:18 -0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,15 @@
 	import Coords from './lib/Coords.svelte';
 	import About from './lib/About.svelte';
 	import FloodButton from './lib/FloodButton.svelte';
-  import Stats from './lib/Stats.svelte';
+	import Stats from './lib/Stats.svelte';
+	import Help form './lib/Help.svelte';
 </script>
 
 <main>
 	<Mapbox />
 	<Coords />
 	<About />
-  <FloodButton />
-  <Stats/>
+	<FloodButton />
+	<Stats/>
+	<Help />
 </main>
